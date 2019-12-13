0a519b47990020da66f6a514b252cdc2839129c0
Joe Maples <joe@maples.dev>
2019-12-13 16:37:11 -0500

    config: Tune PID to fix thermal runaway
    Signed-off-by: Joe Maples <joe@maples.dev>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -480,9 +480,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender
-  #define DEFAULT_Kp 17.66
-  #define DEFAULT_Ki 1.00
-  #define DEFAULT_Kd 78.12
+  #define DEFAULT_Kp 21.73
+  #define DEFAULT_Ki 1.54
+  #define DEFAULT_Kd 76.55
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
