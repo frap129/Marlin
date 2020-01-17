8594506dcbcb98db224ac7f37359e469d42abac4
Joe Maples <joe@maples.dev>
2020-01-16 19:12:45 -0500

    config: Increment PWM scale
    Signed-off-by: Joe Maples <joe@maples.dev>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2106,7 +2106,7 @@
 // However, control resolution will be halved for each increment;
 // at zero value, there are 128 effective control positions.
 // :[0,1,2,3,4,5,6,7]
-#define SOFT_PWM_SCALE 0
+#define SOFT_PWM_SCALE 1
 
 // If SOFT_PWM_SCALE is set to a value higher than 0, dithering can
 // be used to mitigate the associated resolution loss. If enabled,
