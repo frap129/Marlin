0c2f0eb1278ddce934000c9955eff6f3e2d1d1cc
Joe Maples <joe@maples.dev>
2020-01-21 23:26:32 -0500

    config_adv: Custom leveling points
    Tried to eyeball them close to the leveling knobs, the front right one was tricky with bltouch on the hero me
    
    Signed-off-by: Joe Maples <joe@maples.dev>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1471,12 +1471,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  //#define PROBE_PT_1_X 15
-  //#define PROBE_PT_1_Y 180
-  //#define PROBE_PT_2_X 15
-  //#define PROBE_PT_2_Y 20
-  //#define PROBE_PT_3_X 170
-  //#define PROBE_PT_3_Y 20
+  #define PROBE_PT_1_X 66
+  #define PROBE_PT_1_Y 71
+  #define PROBE_PT_2_X 148
+  #define PROBE_PT_2_Y 45
+  #define PROBE_PT_3_X 138
+  #define PROBE_PT_3_Y 141
 #endif
 
 /**
